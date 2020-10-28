af551c2f16a833394a132bd60489274f47fcd669
jess-smith49 <js.jessicasmith774@gmail.com>
2020-10-28 10:09:47 -0500

    fetch, functions, search

---
+++ assets/script.js
@@ -0,0 +1,117 @@
+//Function to display Current Weather
+function getCurrentWeather(){
+    var searchTermEl = $("#city-name").val().trim();
+    event.preventDefault();
+    
+    fetch (
+        `http://api.openweathermap.org/data/2.5/weather?q=${searchTermEl}&appid=519a795400f3f1c248480dfcc8e3bf80&units=imperial`
+        )
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+    
+        let currentWeatherSection = $("#current-weather");
+    
+    
+        let cityName = data.name;
+        let cityIcon = data.weather.icon;
+        //have to get to normal measure
+        let cityTemperature = data.main.temp;
+        let cityHumidity = data.main.humidity;
+        let cityWindSpeed = data.wind.speed;
+        //let cityUV = 
+        let currentWeatherData = 
+        `
+        <div class = "border">
+        <div>
+            <h2>${cityName} 
+            DATE
+            <img src = ${cityIcon}>
+            </h2>
+        </div>
+        <div>Temperature: ${cityTemperature}</div>
+        <div>Humidity: ${cityHumidity} %</div>
+        <div>Wind Speed: ${cityWindSpeed} MPH</div>
+        </div>
+        </br>
+        `
+        
+        currentWeatherSection.append(currentWeatherData);
+            
+        });
+    
+    };
+    
+    
+    //Function to display Daily Forecast
+    function dailyForecast(){
+        //get element input
+        var searchTermEl = $("#city-name").val().trim();
+        event.preventDefault();
+        //get data for 5 day forecast
+        fetch (
+            `http://api.openweathermap.org/data/2.5/forecast?q=${searchTermEl}&appid=519a795400f3f1c248480dfcc8e3bf80&units=imperial`
+            )
+            .then(function(response){
+                return response.json();
+            })
+            .then(function(data){
+                console.log(data);
+                        //get the class for section
+                        let dailyForecastSection = $("#card-container");
+    
+                        //data
+                        let forecastTemp = data.list[0].main.temp;
+                        //console.log(forecastTemp);
+    
+                        //let forecastImg = data.list.weather[0].icon;
+                        //console.log(forecastImg);
+                        
+                        let forecastHumidity = data.list[0].main.humidity;
+                        //console.log(forecastHumidity);
+    
+                        //for loop for forecast
+                        for (var i = 0; i < 5; i ++){
+                        //creating the cards
+                        let dailyCards = `
+                        <div class = "row">
+                        <div class = "col-sm-6"
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title">Date</h5>
+                                <p class="card-text">Temp: ${forecastTemp}</p>
+                                <p class="card-text">Humiditiy: ${forecastHumidity}</p>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                        `
+                        //appending the card
+                        dailyForecastSection.append(dailyCards);
+                    }
+                
+    
+            })
+    };
+    
+    
+    
+    $(".search").click(function(){
+        //console.log("clicked");
+        getCurrentWeather(); 
+        dailyForecast();
+    
+        var searchTermEl = $("#city-name").val().trim();
+        event.preventDefault();
+    
+        //save to search history
+        var previousSearch = $("#previous-search");
+        let searchHistory = `
+        <li class = "border">${searchTermEl}</li>
+        </br>
+        `
+        previousSearch.append(searchHistory);
+    });
+    
